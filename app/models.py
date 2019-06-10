f1313362eba3ba22a6368fc3928c75a966b20890
Sara Palmer <sara.palmer@emory.edu>
2019-06-10 12:31:41 -0400

    update dropdowns, add one field

--- app/models.py
+++ app/models.py
@@ -41,12 +41,35 @@ class TextUnit(models.Model):
     ('Back Cover', 'Back Cover'),
     )
 
+    SUBSECTION_CHOICES = (
+    ('Sub-committee', 'Sub-committee'),
+    ('Committee', 'Committee'),
+    ('Introduction', 'Introduction'),
+    ('Lesson I', 'Lesson I'),
+    ('Lesson II', 'Lesson II'),
+    ('Lesson III', 'Lesson III'),
+    ('Lesson IV', 'Lesson IV'),
+    ('Lesson V', 'Lesson V'),
+    ('Lesson VI', 'Lesson VI'),
+    ('Lesson VII', 'Lesson VII'),
+    ('Lesson VIII', 'Lesson VIII'),
+    ('Lesson IX', 'Lesson IX'),
+    ('Lesson X', 'Lesson X'),
+    ('Dynamics', 'Dynamics'),
+    ('Solmization-Greek Notation', 'Solmization-Greek Notation'),
+    ('The Voice in Singing', 'The Voice in Singing'),
+    ('Intervals', 'Intervals'),
+    ('Miscellaneous', 'Miscellaneous'),
+    ('Metre', 'Metre'),
+    ('Signs Used in Music', 'Signs Used in Music'),
+    ('Additional Index', 'Additional Index'),
+    )
 
     page = models.ForeignKey('Page', blank=False, null=True, on_delete=models.CASCADE, help_text='Relevant page from Pages table')
     seq = models.PositiveIntegerField(blank=False, null=True, help_text='Number indicating where on the page this text unit falls')
     value = models.TextField(blank=False, null=True, help_text='The content of the unit, or a description of it')
     section = models.CharField(max_length=255, blank=False, null=True, help_text='Name of the section of the book', choices=SECTION_CHOICES)
-    subsection = models.CharField(max_length=255, blank=True, null=True, help_text='Name of a subsection of the book, e.g. a section of the Rudiments')
+    subsection = models.CharField(max_length=255, blank=True, null=True, help_text='Name of a subsection of the book, e.g. a section of the Rudiments', choices=SUBSECTION_CHOICES)
 
     def __str__(self):
         return "%s - %s - %s" % (self.section, self.page, self.seq)
@@ -58,8 +81,14 @@ class Song(models.Model):
         (4, '4'),
     )
 
+    PAGE_PLACEMENT_CHOICES = (
+        ('a', 'a'),
+        ('b', 'b'),
+    )
+
     page_start = models.ForeignKey('Page', blank=False, null=True, related_name='page_start', help_text='Starting page of song from Pages', on_delete=models.CASCADE)
     page_end = models.ForeignKey('Page', blank=False, null=True, related_name = 'page_end', help_text='Last page of song from Pages; should be same as the page start if a half-page or one-page song', on_delete=models.CASCADE)
+    page_placement = models.CharField(max_length=1, blank=True, null=True, choices=PAGE_PLACEMENT_CHOICES, help_text='Only has a value if it is one of two songs that begin on a page. Use "A" for top, "B" for bottom.')
     title = models.CharField(max_length=255, blank=False, null=True)
     lines = models.PositiveIntegerField(blank=False, null=True, choices=LINES_CHOICES)
 
@@ -68,10 +97,14 @@ class Song(models.Model):
 
 
 class Book(models.Model):
+    YEAR_CERTAINTY_CHOICES = (
+        ('circa', 'circa'),
+    )
+
     title = models.CharField(max_length=255, blank=False, null=True)
     abbreviated_title = models.CharField(max_length=10, blank=False, null=True)
     year_range_start = models.PositiveIntegerField(validators=[MinValueValidator(1800), MaxValueValidator(2000)], help_text='Year of publication if known or first possible year of publication otherwise', blank=True, null=True)
-    year_certainty = models.CharField(max_length=255, help_text='Way of indicating “circa” or the like', blank=True, null=True)
+    year_certainty = models.CharField(max_length=255, help_text='If year of publication is uncertain, use circa.', blank=True, null=True, choices=YEAR_CERTAINTY_CHOICES)
     year_range_end = models.PositiveIntegerField(validators=[MinValueValidator(1800), MaxValueValidator(2000)], help_text='Last possible year of publication if precise date not known', blank=True, null=True)
     place = models.CharField(max_length=255, help_text='Place of publication', blank=True, null=True)
     publisher = models.CharField(max_length=255, blank=True, null=True)
@@ -106,10 +139,10 @@ class SourceRelationship(models.Model):
         ('scriptural citation', 'scriptural citation'),
         ('attribution', 'attribution'),
         ('song', 'song'),
-        ('hymn text', 'hymn text'),
+        ('hymn', 'hymn'),
         ('alto', 'alto'),
         ('historical note', 'historical note'),
-        ('rudiments', 'rudiments'),
+        ('other content', 'other content'),
     )
 
     TYPE_CHOICES = (
@@ -120,7 +153,7 @@ class SourceRelationship(models.Model):
     )
 
     song = models.ForeignKey('Song', blank=True, null=True, on_delete=models.CASCADE)
-    text_unit = models.ForeignKey('TextUnit', blank=True, null=True, on_delete=models.CASCADE)
+    text_unit = models.ForeignKey('TextUnit', blank=True, null=True, on_delete=models.CASCADE, help_text = "Section - Page - Sequence")
     book = models.ForeignKey('Book', help_text='Book that serves as source for song/text unit', blank=False, null=True, on_delete=models.CASCADE)
     sr_category = models.CharField(max_length=255, help_text='Category of relationship', blank=False, null=True, choices=CATEGORY_CHOICES)
     sr_type = models.CharField(max_length=255, help_text='Type of the relationship', blank=True, null=True, choices=TYPE_CHOICES)
@@ -135,18 +168,26 @@ class OSHPrintingSequence(models.Model):
 
 class PrintingRelationship(models.Model):
     CATEGORY_CHOICES = (
-    #placeholder for values
+    ('scriptural citation', 'scriptural citation'),
+    ('attribution', 'attribution'),
+    ('song', 'song'),
+    ('hymn', 'hymn'),
+    ('alto', 'alto'),
+    ('historical note', 'historical note'),
+    ('other content', 'other content'),
     )
     TYPE_CHOICES = (
-    #placeholder for values
+    ('correction', 'correction'),
+    ('addition', 'addition'),
+    ('alteration', 'alteration'),
     )
 
     osh_printing_sequence = models.ForeignKey('OSHPrintingSequence', blank=False, null=True, on_delete=models.CASCADE, help_text="Printing where change introduced")
     page = models.ForeignKey('Page', blank=True, null=True, on_delete=models.CASCADE)
     song = models.ForeignKey('Song', blank=True, null=True, on_delete=models.CASCADE)
-    text_unit = models.ForeignKey('TextUnit', blank=True, null=True, on_delete=models.CASCADE)
-    pr_category = models.CharField(max_length=255, blank=False, null=True) # add choices="CATEGORY_CHOICES"
-    pr_type = models.CharField(max_length=255, blank=True, null=True) # add choices="TYPE_CHOICES"
+    text_unit = models.ForeignKey('TextUnit', blank=True, null=True, on_delete=models.CASCADE, help_text = "Section - Page - Sequence")
+    pr_category = models.CharField(max_length=255, blank=False, null=True, choices=CATEGORY_CHOICES)
+    pr_type = models.CharField(max_length=255, blank=True, null=True, choices=TYPE_CHOICES)
 
 class Contribution(models.Model):
     CONTRIBUTION_CHOICES = (
